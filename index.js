579e70ae4d84e85e5a5a2a3437185f82d5639ad8
Julien <mianojulien57@gmail.com>
2022-07-20 01:23:27 +0200

    exerises

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+let page = document.getElementsByClassName("page");
 
+let title = [
+  {
+    name: "julien",
+    age: 36,
+    adresse: "4 rue du 7 septembre",
+  },
+];
+
+document.body.div.page.innerHTML += title.map(
+  (test) => `
+
+<h1 class="name">${test.name}</h1>
+
+
+`
+);
