b75b8bb9127a7089898e7823b2d81b2b6e7ad17a
Julien <mianojulien57@gmail.com>
2022-07-20 07:55:43 +0200

    modif

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const page = document.querySelector(".page");
 const mySpan = document.querySelector(".mySpan");
+const mySpan2 = document.querySelector(".mySpan2");
 
 document.addEventListener("keypress", (e) => {
-  mySpan.innerHTML += e.key;
+  if (e.key) {
+    mySpan.innerHTML += e.key;
+  }
 });
