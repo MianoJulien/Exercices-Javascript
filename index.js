b2d273f20d794c7669fcd052e90a0fe04cf896f6
Julien <mianojulien57@gmail.com>
2022-07-22 20:59:13 +0200

    ajout de couleur aléatoire

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ document.addEventListener("keypress", (e) => {
     mySpan.innerHTML += e.key;
   }
 });
+
+function getColor() {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+
+  page.style.background = `rgb(${r},${g},${b})`;
+}
+
+setInterval(getColor, 5000);
