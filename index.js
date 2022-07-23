63683947e7692b27ebf9335e823dc42594201ee9
Julien <mianojulien57@gmail.com>
2022-07-23 06:47:53 +0200

    ajout d'un span et d'un set interval pour la couleur

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const page = document.querySelector(".page");
 const mySpan = document.querySelector(".mySpan");
 const mySpan2 = document.querySelector(".mySpan2");
+page.innerHTML += `<span class="addSpan">hello</span>`;
+const addSpan = document.querySelector(".addSpan");
 
 document.addEventListener("keypress", (e) => {
   if (e.key) {
@@ -16,4 +18,11 @@ function getColor() {
   page.style.background = `rgb(${r},${g},${b})`;
 }
 
+function getColor2() {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  addSpan.style.color = `rgb(${r},${g},${b})`;
+}
 setInterval(getColor, 5000);
+setInterval(getColor2, 1000);
