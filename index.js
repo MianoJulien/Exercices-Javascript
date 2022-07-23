62e0c654d4bf075a342d07b988b349df472a55b1
Julien <mianojulien57@gmail.com>
2022-07-23 07:06:39 +0200

    ajout de couleur aleatoire sur mon nom

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const page = document.querySelector(".page");
 const mySpan = document.querySelector(".mySpan");
 const mySpan2 = document.querySelector(".mySpan2");
-page.innerHTML += `<span class="addSpan">hello</span>`;
+page.innerHTML += `<span class="addSpan">couleur aléatoire</span>`;
 const addSpan = document.querySelector(".addSpan");
+const href = document.querySelector("a");
 
 document.addEventListener("keypress", (e) => {
   if (e.key) {
@@ -23,6 +24,13 @@ function getColor2() {
   const g = Math.floor(Math.random() * 255);
   const b = Math.floor(Math.random() * 255);
   addSpan.style.color = `rgb(${r},${g},${b})`;
+  href.style.color = `rgb(${r},${g},${b})`;
 }
 setInterval(getColor, 5000);
 setInterval(getColor2, 1000);
+
+document.addEventListener("scroll", () => {
+  if (srollY > 1) {
+    header.style.visibility = "hidden";
+  }
+});
